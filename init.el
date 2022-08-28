41d9fb3a14c7dd6ec20d8ba3a775a36c8e0496f2
OzzyNeto <ozzynetocodes@gmail.com>
2022-08-28 17:51:04 -0400

    First commit. Added config.org and init.el files.

---
+++ init.el
@@ -0,0 +1,44 @@
+(require 'package)                   ; Bring in to the environment all package management functions
+
+
+;; A list of package repositories
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org"   . "https://orgmode.org/elpa/")
+                         ("elpa"  . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)                 ; Initializes the package system and prepares it to be used
+
+(unless package-archive-contents     ; Unless a package archive already exists,
+  (package-refresh-contents))        ; Refresh package contents so that Emacs knows which packages to load
+
+
+;; Initialize use-package on non-linux platforms
+(unless (package-installed-p 'use-package)        ; Unless "use-package" is installed, install "use-package"
+  (package-install 'use-package))
+
+(require 'use-package)                            ; Once it's installed, we load it using require
+
+;; Make sure packages are downloaded and installed before they are run
+;; also frees you from having to put :ensure t after installing EVERY PACKAGE.
+(setq use-package-always-ensure t)
+
+
+;; Org babel setup literate config file
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
+ '(package-selected-packages
+   '(sudo-edit hungry-delete switch-window avy doom-themes marginalia vertico rainbow-delimiters which-key vterm use-package rainbow-mode org-superstar gruvbox-theme ef-themes beacon))
+ '(tab-always-indent 'complete))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
